c1d77360c070698abae97e0007269b82131cfc2f
AJAY POLY <ajay@hamon.in>
2022-03-14 15:55:35 +0530

    create postfix test  for alpha numerical

--- PostFix/postfix_program.py
+++ PostFix/postfix_program.py
@@ -1,9 +1,14 @@
 
+
+
 def postfix(s):
     stack = []
     for i in s :
         if i.isdigit():
             stack.append(int(i))
+
+        elif s.isalpha(): 
+            return None  
 
         elif i== "+":
             num1 = stack.pop()
@@ -24,9 +29,6 @@ def postfix(s):
             num1 = stack.pop()
             num2 = stack.pop()
             stack.append(num1*num2)
-
-        else :
-            return None
 
     return stack.pop()
 
--- PostFix/test_postfix.py
+++ PostFix/test_postfix.py
@@ -2,10 +2,16 @@ from postfix_program import postfix
 
 
 def test_postfix_int():
-    ret = postfix("231+-")
-    assert ret ==  2
+    ret = postfix("23331+-*/")
+    assert ret ==  1.5
 
 
 def test_postfix_alpha():
     ret = postfix("abcd")
     assert ret ==  None
+
+
+def test_postfix_alphanum():
+    ret = postfix("123abc+-")
+    assert ret ==  4
+
